aa9b1be249dec44231e0986285bcff04a935785e
Jérémy Lal <kapouer@melix.org>
2015-01-07 02:47:03 +0100

    Implement a live cache, rewrite so raja can plug into it

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ var escapeStringRegexp = require('escape-string-regexp');
 var request = require('request');
 
 var Dom = module.exports = function(model, options) {
+	if (!Dom.pool) Dom.pool = Dom.initPool(Dom.settings);
 	var h = new Handler(model, options);
 	return h.chainable;
 };
@@ -14,7 +15,7 @@ Dom.Handler = Handler;
 
 Dom.settings = {
 	min: 1,
-	max: 8,
+	max: 16,
 	idleTimeoutMillis: 30000,
 	refreshIdle: false,
 	display: 0,
@@ -35,53 +36,23 @@ Dom.use = function(mw) {
 	return Dom;
 };
 
-function Handler(url, options) {
-	this.viewUrl = url;
+function Handler(viewUrl, options) {
+	this.viewUrl = viewUrl;
 	this.options = options || {};
 	this.chainable = this.middleware.bind(this);
 	this.chainable.author = this.author.bind(this);
 	this.chainable.use = this.use.bind(this);
 	this.authors = Dom.authors.slice(0);
 	this.users = Dom.users.slice(0);
+	this.pages = {};
+	if (this.init) this.init(); // used by raja
 }
 
 Handler.prototype.middleware = function(req, res, next) {
-	this.url = req.protocol + '://' + req.headers.host + req.url;
-	if (this.url == this.viewUrl) {
-		return next(new Error("The view has the same url as the requested page"));
-	}
-	if (!Dom.pool) Dom.pool = initPool(Dom.settings);
-	queue(1)
-	.defer(this.getView.bind(this), req)
-	.defer(this.getAuthored.bind(this), req, res)
-	.defer(this.getUsed.bind(this), req, res)
-	.defer(this.release.bind(this))
-	.awaitAll(function(err, stack) {
-		if (err) return next(err);
-		res.type('text/html');
-		res.send(this.html);
-	}.bind(this));
-};
-
-Handler.prototype.acquire = function(cb) {
 	var h = this;
-	if (h.page) return cb();
-	Dom.pool.acquire(function(err, page) {
-		h.page = page;
-		cb(err);
-	});
-};
-
-Handler.prototype.getView = function(req, cb) {
-	var h = this;
-	var settings = req.app.settings;
-	if (/https?:/.test(h.viewUrl)) {
-		request(h.viewUrl, function(err, res, body) {
-			if (body) h.viewHtml = body;
-			else err = new Error("Empty initial html in " + h.viewUrl);
-			cb(err);
-		});
-	} else {
+	if (!/https?:/.test(h.viewUrl)) {
+		// absolute path for h.viewUrl
+		var settings = req.app.settings;
 		var expressView = new (settings.view)(h.viewUrl, {
 			defaultEngine: 'html',
 			root: settings.views,
@@ -94,64 +65,163 @@ Handler.prototype.getView = function(req, cb) {
 			: 'directory "' + root + '"';
 			var err = new Error('Failed to lookup view "' + h.viewUrl + '" in views ' + dirs);
 			err.view = expressView;
-			return cb(err);
+			return next(err);
 		}
-		fs.readFile(expressView.path, function(err, body) {
-			if (body) h.viewHtml = body;
-			else err = new Error("Empty initial html in " + expressView.path);
+		h.viewUrl = expressView.path;
+	}
+	var url = h.url = req.protocol + '://' + req.headers.host + req.url;
+	if (url == h.viewUrl) {
+		return next(new Error("The view has the same url as the requested page"));
+	}
+	h.instance(url, function(err, inst) {
+		if (err) return next(err);
+		var q = queue(1);
+		if (!inst.html) {
+			if (!inst.authorHtml) {
+				if (!h.viewHtml) {
+					q.defer(h.getView.bind(h), req);
+				}
+				q.defer(h.getAuthored.bind(h), inst, req, res);
+			}
+			q.defer(h.getUsed.bind(h), inst, req, res);
+		}
+		q.defer(h.finish.bind(h), inst, res)
+		.defer(h.gc.bind(h))
+		.awaitAll(function(err, stack) {
+			if (err) return next(err);
+		});
+	});
+};
+
+Handler.prototype.instance = function(url, cb) {
+	var h = this;
+	var inst = h.pages[url];
+	if (!inst) inst = h.pages[url] = {
+		hits: 0,
+		url: url
+	};
+	inst.hits++;
+	inst.atime = Date.now();
+	inst.lock = true;
+	cb(null, inst);
+};
+
+Handler.prototype.finish = function(inst, res, cb) {
+	res.type('text/html');
+	res.send(inst.html);
+	// call getUsed when requesting this url again
+	delete inst.html;
+	inst.lock = false;
+	cb();
+};
+
+Handler.prototype.acquire = function(inst, cb) {
+	if (inst.page) return cb();
+	// before acquiring a new page, make sure we can - pool.availableObjectsCount() > 0
+	// if it's zero, release a non-locked page with lowest (hits, atime) pair
+	this.gc(function() {
+		Dom.pool.acquire(function(err, page) {
+			inst.page = page;
 			cb(err);
 		});
+	});
+};
+
+Handler.prototype.gc = function(cb) {
+	if (Dom.pool.getPoolSize() < Dom.settings.max || Dom.pool.availableObjectsCount() > 0) return cb();
+	var minScore = +Infinity;
+	var minInst;
+	for (var url in this.pages) {
+		var inst = this.pages[url];
+		if (inst.lock) continue;
+		var score = 0;
+		if (inst.score !== undefined) {
+			// allow a score to be set by application
+			score = inst.score;
+		} else {
+			// or use our default scoring
+			score = (inst.weight || 1) * 60000 * inst.hits / (Date.now() - inst.atime);
+		}
+		if (score < minScore) {
+			minScore = score;
+			minInst = inst;
+		}
+	}
+	if (minInst) {
+		this.release(minInst, cb);
 	}
 };
 
-Handler.prototype.load = function(req, cb) {
+Handler.prototype.getView = function(req, cb) {
+	var h = this;
+	if (h.viewHtml) return cb();
+	var loader = /https?:/.test(h.viewUrl) ? h.loadRemote : h.loadLocal;
+	loader.call(h, h.viewUrl, function(err, body) {
+		if (body) h.viewHtml = body;
+		else err = new Error("Empty initial html in " + h.viewUrl);
+		cb(err);
+	});
+};
+
+Handler.prototype.loadLocal = fs.readFile;
+Handler.prototype.loadRemote = function(url, cb) {
+	request(url, function(err, res, body) {
+		cb(err, body);
+	});
+};
+
+Handler.prototype.load = function(inst, req, cb) {
 	var h = this;
 	var opts = {};
 	for (var key in h.options) {
 		opts[key] = h.options[key];
 	}
-	if (!opts.content) opts.content = h.authorHtml;
+	if (!opts.content) opts.content = inst.authorHtml;
 	if (!opts.cookie) opts.cookie = req.get('Cookie');
 	if (opts.console === undefined) opts.console = true;
-	this.acquire(function(err) {
+	this.acquire(inst, function(err) {
 		if (err) return cb(err);
-		if (!cb) h.page.load(h.url, opts);
-		else if (cb) h.page.load(h.url, opts, cb);
+		inst.page.load(inst.url, opts, cb);
 	});
 };
 
-Handler.prototype.getAuthored = function(req, res, cb) {
+Handler.prototype.getAuthored = function(inst, req, res, cb) {
 	var h = this;
+	if (inst.authorHtml) return cb();
 	if (h.authors.length) {
-		h.acquire(function(err) {
+		h.acquire(inst, function(err) {
 			if (err) return cb(err);
-			h.page.preload(h.url, {content: h.viewHtml, console: true});
-			h.processMw(h.authors, req, res);
-			h.page.wait('idle').html(function(err, html) {
+			inst.page.preload(inst.url, {content: h.viewHtml, console: true});
+			h.processMw(inst, h.authors, req, res);
+			inst.page.wait('idle').html(function(err, html) {
 				if (err) return cb(err);
-				h.authorHtml = html;
-				h.page.removeAllListeners();
+				inst.authorHtml = html;
+				inst.page.removeAllListeners();
 				cb();
 			});
 		});
 	} else {
-		h.authorHtml = h.viewHtml;
+		inst.authorHtml = h.viewHtml;
 		cb();
 	}
 };
 
-Handler.prototype.getUsed = function(req, res, cb) {
-	this.load(req);
-	this.processMw(this.users, req, res);
-	this.page.wait('idle').html(function(err, html) {
-		if (err) return cb(err);
-		this.html = html;
-		cb();
-	}.bind(this));
+Handler.prototype.getUsed = function(inst, req, res, cb) {
+	var h = this;
+	if (inst.html) return cb();
+	h.load(inst, req, function(err) {
+		h.processMw(inst, h.users, req, res);
+		inst.page.wait('idle').html(function(err, html) {
+			if (err) return cb(err);
+			inst.html = html;
+			cb();
+		});
+	});
 };
 
-Handler.prototype.release = function(cb) {
-	var page = this.page;
+Handler.prototype.release = function(inst, cb) {
+	delete this.pages[inst.url];
+	var page = inst.page;
 	if (!page) return cb();
 	page.removeAllListeners();
 	page.unload(function(err) {
@@ -160,7 +230,7 @@ Handler.prototype.release = function(cb) {
 	});
 };
 
-Handler.prototype.processMw = function(list, req, res) {
+Handler.prototype.processMw = function(inst, list, req, res) {
 	if (!list || !list.length) return;
 	for (var i=0, mw; i < list.length; i++) {
 		mw = list[i];
@@ -168,7 +238,7 @@ Handler.prototype.processMw = function(list, req, res) {
 			console.error("Empty middleware");
 			continue;
 		}
-		mw(this, req, res);
+		mw(inst, req, res);
 	}
 };
 
@@ -182,7 +252,7 @@ Handler.prototype.author = function(mw) {
 };
 
 
-function initPool(settings) {
+Dom.initPool = function(settings) {
 	var opts = {};
 	for (var prop in settings) opts[prop] = settings[prop];
 	if (opts.debug) {
@@ -209,5 +279,5 @@ function initPool(settings) {
 		});
 	});
 	return pool;
-}
+};
 
