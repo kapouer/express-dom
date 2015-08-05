8c5be073a1805ad3754b64a46261a3a98795c60f
Jérémy Lal <kapouer@melix.org>
2015-08-05 15:47:23 +0200

    Update plugins to use webkitgtk 2 api

--- plugins.js
+++ plugins.js
@@ -2,56 +2,60 @@ var URL = require('url');
 var Path = require('path');
 
 exports.absolute = function(page) {
-	page.wait('ready').run(function() {
-		var dloc = document.location;
-		var base = dloc.protocol + '//' + dloc.host;
-		function absolut(selector, att) {
-			var list = document.querySelectorAll(selector);
-			var node;
-			for (var i=0; i < list.length; i++) {
-				node = list.item(i);
-				var item = node.attributes.getNamedItem(att);
-				if (!item) continue;
-				var uloc = new URL(item.nodeValue, base);
-				item.nodeValue = uloc.href;
+	page.when('ready', function(cb) {
+		this.run(function() {
+			var dloc = document.location;
+			var base = dloc.protocol + '//' + dloc.host;
+			function absolut(selector, att) {
+				var list = document.querySelectorAll(selector);
+				var node;
+				for (var i=0; i < list.length; i++) {
+					node = list.item(i);
+					var item = node.attributes.getNamedItem(att);
+					if (!item) continue;
+					var uloc = new URL(item.nodeValue, base);
+					item.nodeValue = uloc.href;
+				}
 			}
-		}
-		absolut('a', 'href');
-		absolut('img', 'src');
-		absolut('video', 'src');
-		absolut('object', 'src');
-		absolut('link', 'href');
-		absolut('script', 'src');
-		absolut('include', 'src');
+			absolut('a', 'href');
+			absolut('img', 'src');
+			absolut('video', 'src');
+			absolut('object', 'src');
+			absolut('link', 'href');
+			absolut('script', 'src');
+			absolut('include', 'src');
+		}, cb);
 	});
 };
 
 exports.mount = function(page) {
-	page.wait('ready').run(function() {
-		var dloc = document.location;
-		var base = dloc.protocol + '//' + dloc.host;
-		function mount(selector, att) {
-			var list = document.querySelectorAll(selector);
-			var node;
-			for (var i=0; i < list.length; i++) {
-				node = list.item(i);
-				var item = node.attributes.getNamedItem(att);
-				if (!item) continue;
-				var val = item.nodeValue;
-				if (!val || val.charAt(0) == '#') continue;
-				var uloc = new URL(val, base);
-				if (uloc.protocol == dloc.protocol && uloc.host == dloc.host) {
-					item.nodeValue = uloc.pathname + uloc.search + uloc.hash;
+	page.when('ready', function(cb) {
+		this.run(function() {
+			var dloc = document.location;
+			var base = dloc.protocol + '//' + dloc.host;
+			function mount(selector, att) {
+				var list = document.querySelectorAll(selector);
+				var node;
+				for (var i=0; i < list.length; i++) {
+					node = list.item(i);
+					var item = node.attributes.getNamedItem(att);
+					if (!item) continue;
+					var val = item.nodeValue;
+					if (!val || val.charAt(0) == '#') continue;
+					var uloc = new URL(val, base);
+					if (uloc.protocol == dloc.protocol && uloc.host == dloc.host) {
+						item.nodeValue = uloc.pathname + uloc.search + uloc.hash;
+					}
 				}
 			}
-		}
-		mount('a', 'href');
-		mount('img', 'src');
-		mount('video', 'src');
-		mount('object', 'src');
-		mount('link', 'href');
-		mount('script', 'src');
-		mount('include', 'src');
+			mount('a', 'href');
+			mount('img', 'src');
+			mount('video', 'src');
+			mount('object', 'src');
+			mount('link', 'href');
+			mount('script', 'src');
+			mount('include', 'src');
+		}, cb);
 	});
 };
 
