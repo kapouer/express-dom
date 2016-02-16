a62ec6841b2203e59051389aed204b51ce0a2f91
Jérémy Lal <kapouer@melix.org>
2016-02-16 15:59:22 +0100

    Just use node http agent to request

--- examples/snapshot-app.js
+++ examples/snapshot-app.js
@@ -1,10 +1,10 @@
 var app = require('express')();
 var dom = require('..');
-var request = require('request');
 var URL = require('url');
 
 app.get('*', function(req, res, next) {
 	var url = req.query.url;
+	if (!url) return res.sendStatus(404);
 	dom(request(url)).load({
 		plugins: dom.plugins.png
 	}, function(page, state) {
@@ -22,3 +22,17 @@ server = app.listen(process.env.PORT, function(err) {
 	`);
 });
 
+function request(url) {
+	var obj = URL.parse(url);
+	if (!obj.protocol) obj.protocol = 'http';
+	var mod = obj.protocol.startsWith('https') ? require('https') : require('http');
+	obj.headers = {
+		'User-Agent': "Mozilla/5.0"
+	};
+	var ps = new require('stream').PassThrough();
+	mod.request(obj, function(res) {
+		res.setEncoding('utf-8');
+		res.pipe(ps);
+	}).end();
+	return ps;
+}
