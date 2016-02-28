3c2a772bd4ee05a10a4b4b273a7cb446622a7005
Jérémy Lal <kapouer@melix.org>
2016-02-28 01:56:38 +0100

    Update example

--- examples/snapshot-app.js
+++ examples/snapshot-app.js
@@ -2,17 +2,25 @@ var app = require('express')();
 var dom = require('..');
 var URL = require('url');
 
-app.get('*', function(req, res, next) {
-	var url = req.query.url;
-	if (!url) return res.sendStatus(404);
-	dom(request(url)).load({
-		plugins: dom.plugins.png
-	}, function(page, settings, request) {
-		// make sure the page is hosted with the remote url, not the localhost one
-		settings.allow = 'all';
-		request.location = URL.parse(url);
-	})(req, res, next);
-});
+app.get('*', dom(function(settings, request, response) {
+	if (!request.query.url) return response.sendStatus(400);
+	var obj = URL.parse(request.query.url);
+	if (!obj.protocol) obj.protocol = 'http:';
+	settings.location = Object.assign({}, obj);
+	var mod = obj.protocol.startsWith('https') ? require('https') : require('http');
+	obj.headers = {
+		'User-Agent': "Mozilla/5.0"
+	};
+	// here we use a passthrough, but returning a promise can avoid that
+	var ps = new require('stream').PassThrough();
+	mod.request(obj, function(res) {
+		res.setEncoding('utf-8');
+		res.pipe(ps);
+	}).end();
+	settings.input = ps;
+}).load({
+	plugins: dom.plugins.png
+}));
 
 server = app.listen(process.env.PORT, function(err) {
 	if (err) console.error(err);
@@ -23,17 +31,3 @@ server = app.listen(process.env.PORT, function(err) {
 	`);
 });
 
-function request(url) {
-	var obj = URL.parse(url);
-	if (!obj.protocol) obj.protocol = 'http';
-	var mod = obj.protocol.startsWith('https') ? require('https') : require('http');
-	obj.headers = {
-		'User-Agent': "Mozilla/5.0"
-	};
-	var ps = new require('stream').PassThrough();
-	mod.request(obj, function(res) {
-		res.setEncoding('utf-8');
-		res.pipe(ps);
-	}).end();
-	return ps;
-}
