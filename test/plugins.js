7cdd5055c9eba091bffe3c03233421dc5c2318a0
Jérémy Lal <kapouer@melix.org>
2022-06-07 09:12:11 +0200

    Test cookies plugin

--- test/plugins.js
+++ test/plugins.js
@@ -7,6 +7,7 @@ const dom = require('../');
 
 dom.settings.timeout = 10000;
 dom.settings.console = true;
+dom.settings.load.cookies.add('mycookiename');
 
 dom.settings.debug = require('node:inspector').url() !== undefined;
 
@@ -17,6 +18,22 @@ describe("Plugins", function() {
 	before(async () => {
 		const app = express();
 		app.set('views', __dirname + '/public');
+		app.use((req, res, next) => {
+			const header = req.get('cookie');
+			req.cookies = header ? Object.fromEntries( // poor man's cookie parser
+				header.split(';').map(str => str.split('='))
+			) : {};
+			next();
+		});
+		app.get('/protected.json', (req, res, next) => {
+			if (req.cookies.anothercookie) {
+				res.sendStatus(403);
+			} else if (req.cookies.mycookiename == '1') {
+				res.send({ protected: 'mycookievalue' });
+			} else {
+				res.sendStatus(401);
+			}
+		});
 		app.get(/\.(json|js|css|png)$/, (req, res, next) => {
 			if (req.query.delay) {
 				setTimeout(next, parseInt(req.query.delay));
@@ -28,6 +45,7 @@ describe("Plugins", function() {
 
 		app.get('/plugin-status.html', dom().load({ plugins: ['equivs'] }));
 		app.get('/plugin-preload.html', dom().load({ plugins: ['preloads'] }));
+		app.get('/plugin-cookie.html', dom().load());
 
 		app.get(/\.html$/, dom().load());
 
@@ -53,5 +71,15 @@ describe("Plugins", function() {
 		const { statusCode, headers } = await request(`${host}/plugin-preload.html`);
 		assert.equal(statusCode, 200);
 		assert.equal(headers.link, "</css/style.css>;rel=preload;as=style,</js/extern.js>;rel=preload;as=script");
+	});
+
+	it("sets cookie for inner request", async () => {
+		const { statusCode, body } = await request(`${host}/plugin-cookie.html`, {
+			headers: {
+				cookie: 'mycookiename=1;anothercookie=2'
+			}
+		});
+		assert.equal(statusCode, 200);
+		assert.match(await body.text(), /mycookievalue/);
 	});
 });
