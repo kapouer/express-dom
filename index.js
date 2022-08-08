f8b01704f4b68bce5a291c0a5ce9fd553d65c99b
Jérémy Lal <kapouer@melix.org>
2022-08-08 17:38:40 +0200

    Simplify a bit

--- index.js
+++ index.js
@@ -3,10 +3,7 @@ const Handler = require('./lib/handler');
 module.exports = new Proxy(Handler, {
 	apply: (target, thisArg, args) => {
 		const h = new Handler(...args);
-		h.chain.route = fn => {
-			h.route(fn);
-			return h.chain;
-		};
+		h.chain.route = fn => h.route(fn);
 		return h.chain;
 	},
 	get(...args) {
